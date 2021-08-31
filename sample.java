40e77aab8f36060fd77f67d530adf870734e06bb
Yerramsetti R Sai Krishna <65356848+rupaksaikrishnayerramsetti@users.noreply.github.com>
2021-08-31 19:58:49 +0530

    concept of switch
    program to implement switch concept

--- sample.java
+++ sample.java
@@ -3,14 +3,22 @@ public class sample {
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int year,days;
-		int cn=60*24*365;
-		year=a/cn;
-		System.out.print(year+" years ");
-		a=a-(year*cn);
-		int cm=60*24;
-		days=a/cm;
-		System.out.print("and "+days+" days.");
+		int choice;
+        System.out.println("pick one : 1.hi \t 2.hello\t 3.bravo");
+        choice=sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("you said hi");
+                break;
+            case 2:
+                System.out.println("you said hello");
+                break;
+            case 3:
+                System.out.println("you said bravo");
+                break;
+            default:
+            System.out.println("Invalid syntax");
+                break;
+        }
 	}
 }
