e34b758d5e3ad89df881c5486191b67a09ae12e8
Yerramsetti R Sai Krishna <65356848+rupaksaikrishnayerramsetti@users.noreply.github.com>
2021-08-31 20:52:05 +0530

    fibonacci program
    program to implement fibonacci program

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+class Fibonacci{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,sum,i;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    Scanner s=new Scanner(System.in);
+		int count=s.nextInt();
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  sum=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+  
+}}
