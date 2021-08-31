701a39a1052aad2c834ec97b53dd170cfbd50b1a
Yerramsetti R Sai Krishna <65356848+rupaksaikrishnayerramsetti@users.noreply.github.com>
2021-08-31 19:30:55 +0530

    minutes into number of years and days
    program to convert minutes into number of years and days

--- sample.java
+++ sample.java
@@ -1,5 +1,16 @@
-class sample{
-    public static void main(String a[]){
-        System.out.println("hello world");
-    }
+import java.util.*;
+public class sample {
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int year,days;
+		int cn=60*24*365;
+		year=a/cn;
+		System.out.print(year+" years ");
+		a=a-(year*cn);
+		int cm=60*24;
+		days=a/cm;
+		System.out.print("and "+days+" days.");
+	}
 }
